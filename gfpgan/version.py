8c98f6a95cf286788d7ed6d9f719d68c412a6cad
Tam L <tamlam@wearetopgroup.com>
2023-12-19 22:49:21 +0700

    refactor: update facefusion 2.1

--- gfpgan/version.py
+++ gfpgan/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Dec  6 22:22:57 2023
+# TIME: Tue Dec 19 21:18:08 2023
 __version__ = '1.3.8'
-__gitsha__ = '3ef4073'
+__gitsha__ = 'ccbfa54'
 version_info = (1, 3, 8)
